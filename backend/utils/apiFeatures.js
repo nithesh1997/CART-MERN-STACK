057632b074c744e1063a2bcd4790d3ab69c63166
nithesh1997 <nitheshroshan007@gmail.com>
2024-12-30 19:27:06 +0530

    created price filtering option in the api

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -21,21 +21,26 @@ class APIFeatures{
             // Create a shallow copy of the query string object to avoid mutating the original query parameters.
             const queryStrCopy = { ...this.queryStr };
     
-            // Log the query parameters before removing unwanted fields for debugging purposes.
-            // console.log("Before removing fields:", queryStrCopy);
-    
             // Define an array of fields to be removed from the query string.
-            // These fields are not required for filtering the database query.
-            const removeFields = ['keyword', 'limit', 'page'];
+            const removeFields = ['keyword', 'limit', 'page']; // category
     
             // Loop through the array of fields to be removed and delete them from the copied query string object.
             removeFields.forEach(field => delete queryStrCopy[field]);
+
+            let queryStr = JSON.stringify(queryStrCopy);
+
+            // ?price[lt]=500 // less then 500
+            // ?price[gt]=1000 // greater then 1000
+            // ?price[lte]=500 // less then or equal 500
+            // ?price[gte]=1000 // greater then or equal 1000
+            // Example format {"price":{"$lt":"200","$gte":"100"}}
+            queryStr = queryStr.replace(/\b(gt|gte|lt|lte)/g, (match) => `$${match}`)
     
             // Log the modified query parameters after removing the unwanted fields.
             // console.log("After removing fields:", queryStrCopy);
     
             // Use the modified query parameters to further filter the MongoDB query.
-            this.query.find(queryStrCopy);
+            this.query.find(JSON.parse(queryStr));
     
         } catch (error) {
             // Catch any errors during the filtering process and log them to the console.
