8f3dda05b5094e79647dd2bdfd81e284f32ab3a4
nithesh1997 <nitheshroshan007@gmail.com>
2025-01-03 17:43:44 +0530

    Added myOrders method for user order API in orderController.js

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { newOrder, getSingleOrder } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
 const { isAuthenticatedUser } = require("../middlewares/authenticate");
 const router = express.Router();
 
 router.route('/order/new').post(isAuthenticatedUser ,newOrder)
 router.route('/order/:id').get(isAuthenticatedUser ,getSingleOrder)
+router.route('/myorders').get(isAuthenticatedUser , myOrders)
 
 module.exports = router
