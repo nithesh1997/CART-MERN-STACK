ea64de2e6395cbc226691f452a647ce643b834ec
nithesh1997 <nitheshroshan007@gmail.com>
2025-01-03 07:23:53 +0530

    created update profile in the authController.js and added err.statusCode production error

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ const {
     resetPassword, 
     getUserProfile,
     changePassword,
+    updateProfile,
 } = require('../controllers/authController');
 const { isAuthenticatedUser } = require('../middlewares/authenticate');
 const router = express.Router();
@@ -16,7 +17,8 @@ router.route('/login').post(loginUser);
 router.route('/logout').post(logoutUser);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').post(resetPassword);
-router.route('/myProfile').get(isAuthenticatedUser, getUserProfile)
-router.route('/password/change').get(isAuthenticatedUser, changePassword)
+router.route('/myProfile').get(isAuthenticatedUser, getUserProfile);
+router.route('/password/change').put(isAuthenticatedUser, changePassword);
+router.route('/update').put(isAuthenticatedUser, updateProfile);
 
 module.exports = router;
