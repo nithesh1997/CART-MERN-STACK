061d6f269c162fd0ef386547299275ec22ba927a
nithesh1997 <nitheshroshan007@gmail.com>
2025-01-08 21:04:47 +0530

    created api of createReview method in the productController.js

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, createReview } = require("../controllers/productController");
 const router = express.Router();
 const { isAuthenticatedUser,authorizeRoles } = require('../middlewares/authenticate');
 
@@ -8,6 +8,7 @@ router.route('/product/:id')
                         .get(getSingleProduct)
                         .put(updateProduct)
                         .delete(deleteProduct);
+router.route('/review').put(isAuthenticatedUser, createReview)
 
 // Admin routes 
 router.route('/admin/product/new').post(isAuthenticatedUser , authorizeRoles('admin'), newProduct);
