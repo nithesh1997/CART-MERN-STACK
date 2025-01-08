719c2b1172e0c0a3ab67106e48166f5fea4d2c51
nithesh1997 <nitheshroshan007@gmail.com>
2025-01-08 14:49:23 +0530

    created deleteOrder api in the orderController.js

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { newOrder, getSingleOrder, myOrders, allOrders, updateOrder } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders, allOrders, updateOrder, deleteOrder } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/authenticate");
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.route('/myorders').get(isAuthenticatedUser , myOrders)
 
 // Admin Routes
 router.route('/all/orders').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders);
-router.route('/order/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder);
+router.route('/order/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
+                          .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
 
 module.exports = router
