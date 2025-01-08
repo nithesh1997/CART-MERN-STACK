db44a6aae9ea2f0025e720504ea5fca1c8d00cbd
nithesh1997 <nitheshroshan007@gmail.com>
2025-01-08 13:44:58 +0530

    created admin updateOrder method in the orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 const catchAsyncError = require('../middlewares/catchAsyncError');
 const Order = require('../models/orderModel');
+const Product = require('../models/productModel');
 const ErrorHandler = require('../utils/errorHandler');
 
 // Create New Order - api/v1/order/new
@@ -78,3 +79,32 @@ exports.allOrders = catchAsyncError(async (req, res, next) => {
     })
 
 })
+
+// Admin: Update Order - (order status and delivery date) -> api/v1/order/:id
+exports.updateOrder = catchAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if(order.orderStatus == "Delivered"){
+        return next(new ErrorHandler('Order has been already delivered!', 400));
+    }
+
+    // Updating the product stock of each order item
+    order.orderItems.map(async orderItem => {
+      await  updateStock(orderItem.product, orderItem.quantity)
+    })
+
+    order.orderStatus = req.body.orderStatus;
+    order.deliveredAt = Date.now();
+    await order.save();
+
+    res.status(200).json({
+        success: true
+    })
+})
+
+async function updateStock (productId, quantity){
+    const product = await Product.findById(productId)
+
+    product.stock = product.stock - quantity;
+    product.save({validateBeforeSave: false});
+}
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { newOrder, getSingleOrder, myOrders, allOrders } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders, allOrders, updateOrder } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/authenticate");
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.route('/order/:id').get(isAuthenticatedUser ,getSingleOrder)
 router.route('/myorders').get(isAuthenticatedUser , myOrders)
 
 // Admin Routes
-router.route('/all/orders').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders)
+router.route('/all/orders').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders);
+router.route('/order/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder);
 
 module.exports = router
